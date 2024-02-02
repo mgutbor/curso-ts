4662fd3617d2b2a5538b165eae9a5c3d4658bbb0
mgutbor <mgutbor@gmail.com>
2024-02-02 20:49:13 +0100

    Curso TS - 03.7 - Enum

---
+++ 01-bases/tipos/enum.ts
@@ -0,0 +1,15 @@
+(() => {
+
+  enum AudioLevel {
+    min = 1,
+    medium,
+    max = 10
+  }
+
+  const currentAudioLevel = AudioLevel.medium;
+
+  console.log({currentAudioLevel});
+  
+  
+  
+})()
