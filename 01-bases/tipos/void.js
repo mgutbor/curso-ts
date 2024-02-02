2d836c8a7d38a6c81829c1ad485991576626949a
mgutbor <mgutbor@gmail.com>
2024-02-02 21:00:23 +0100

    Curso TS - 03.8 - void

---
+++ 01-bases/tipos/void.js
@@ -0,0 +1,11 @@
+"use strict";
+(function () {
+    function llamarBatman() {
+    }
+    var llamarSuperman = function () {
+    };
+    var a = llamarBatman();
+    console.log(a);
+    var b = llamarSuperman();
+    console.log(b);
+})();
---
+++ 01-bases/tipos/void.ts
@@ -0,0 +1,22 @@
+(() => {
+
+  function llamarBatman():void{
+
+  }
+  
+  const llamarSuperman = ():void => {
+
+  }
+
+  const a = llamarBatman();
+
+  console.log(a);
+
+  const b = llamarSuperman();
+
+  console.log(b);
+  
+  
+  
+
+})()
