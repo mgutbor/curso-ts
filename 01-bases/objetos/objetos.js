b9c0e449208deec95527b6fe0d9008687731fb1a
mgutbor <mgutbor@gmail.com>
2024-02-04 09:26:42 +0100

    Curso TS - 05.01 - objetos basicos

---
+++ 01-bases/objetos/objetos.js
@@ -0,0 +1,13 @@
+"use strict";
+(function () {
+    var heroe = {
+        nombre: 'Clark Kent',
+        edad: 37,
+        poderes: ['Volar', 'Super fuerza']
+    };
+    heroe = {
+        nombre: 'Otro nombre',
+        edad: 12,
+        poderes: ['Ninguno']
+    };
+})();
---
+++ 01-bases/objetos/objetos.ts
@@ -0,0 +1,15 @@
+(() => {
+
+  let heroe = {
+    nombre: 'Clark Kent',
+    edad: 37,
+    poderes: ['Volar', 'Super fuerza']
+  }
+
+  heroe = {
+    nombre: 'Otro nombre',
+    edad: 12,
+    poderes: ['Ninguno']
+  }
+  
+})()
