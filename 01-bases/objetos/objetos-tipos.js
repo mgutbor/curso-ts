76a1d9077607b19fbe49f05f40be54c2f2922a9b
mgutbor <mgutbor@gmail.com>
2024-02-04 09:37:07 +0100

    Curso TS - 05.02 - objetos con tipos especificos

---
+++ 01-bases/objetos/objetos-tipos.js
@@ -0,0 +1,17 @@
+"use strict";
+(function () {
+    var heroe = {
+        nombre: 'Clark Kent',
+        edad: 37,
+        poderes: ['Volar', 'Super fuerza']
+    };
+    heroe = {
+        nombre: 'Otro nombre',
+        // edad: 12,
+        poderes: ['Ninguno'],
+        // getNombre(){
+        //   return this.nombre;
+        // }
+    };
+    console.log({ heroe: heroe });
+})();
---
+++ 01-bases/objetos/objetos-tipos.ts
@@ -0,0 +1,21 @@
+(() => {
+
+  let heroe: { nombre: string, edad?: number, poderes: string[] } = {
+    nombre: 'Clark Kent',
+    edad: 37,
+    poderes: ['Volar', 'Super fuerza']
+  }
+
+  heroe = {
+    nombre: 'Otro nombre',
+    // edad: 12,
+    poderes: ['Ninguno'],
+    // getNombre(){
+    //   return this.nombre;
+    // }
+  }
+
+  console.log({heroe});
+  
+  
+})()
