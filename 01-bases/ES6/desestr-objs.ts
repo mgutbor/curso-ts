e9dcef051ef0c6b73e2e7bc89c1462af947f160a
mgutbor <mgutbor@gmail.com>
2024-02-05 20:18:50 +0100

    Curso TS - 07.02 - desestructuracion de objetos

---
+++ 01-bases/ES6/desestr-objs.ts
@@ -0,0 +1,28 @@
+(() => {
+
+  type heroe = {
+    superman: string;
+    spiderman: string;
+    batman: string;
+    activos: boolean;
+    poderTotal: number;
+  }
+
+  const heroes:heroe = {
+    superman: 'Clark Kent',
+    spiderman: 'Peter Parker',
+    batman: 'Bruce Wayne',
+    activos: true,
+    poderTotal: 1500.32987432
+  }
+
+  // const { superman, activos, poderTotal } = heroes;
+  // console.log(superman.toUpperCase(), poderTotal.toFixed(2));
+
+  const imprimirPoderHeroes = ({poderTotal, ...resto}:heroe) => console.log(resto.superman);
+
+  imprimirPoderHeroes(heroes);
+  
+  
+
+})()
--- 01-bases/main.js
+++ 01-bases/main.js
@@ -1,7 +1,32 @@
 "use strict";
-(function () {
-    var nombre = 'Manuel';
-    var getname = function () {
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
+                t[p[i]] = s[p[i]];
+        }
+    return t;
+};
+(() => {
+    const heroes = {
+        superman: 'Clark Kent',
+        spiderman: 'Peter Parker',
+        batman: 'Bruce Wayne',
+        activos: true,
+        poderTotal: 1500.32987432
+    };
+    const imprimirPoderHeroes = (_a) => {
+        var { poderTotal } = _a, resto = __rest(_a, ["poderTotal"]);
+        return console.log(resto.superman);
+    };
+    imprimirPoderHeroes(heroes);
+})();
+(() => {
+    const nombre = 'Manuel';
+    const getname = () => {
         console.log('viejo getName');
     };
     getname();
