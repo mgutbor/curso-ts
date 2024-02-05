0c4060cd9ae777c40a5d115bad22da2826f239e1
mgutbor <mgutbor@gmail.com>
2024-02-05 20:01:01 +0100

    Curso TS - 07.01 - const-let

---
+++ 01-bases/ES6/let-var.ts
@@ -0,0 +1,15 @@
+(() => {
+
+  //  console.log(a);
+
+   const nombre: string = 'Manuel';
+
+   const getname = (): void => {
+    console.log('viejo getName');
+   }
+   
+  //  getname = () => { console.log('nuevoGetName');}
+  
+  getname();
+
+})()
