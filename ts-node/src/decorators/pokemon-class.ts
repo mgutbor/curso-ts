bc2f38316edc460a617191a07a704e3df30e3594
mgutbor <mgutbor@gmail.com>
2024-02-11 13:36:54 +0100

    Curso TS - 12.05 - decoradores de metodos

--- ts-node/src/decorators/pokemon-class.ts
+++ ts-node/src/decorators/pokemon-class.ts
@@ -15,8 +15,20 @@ const blockPrototype = function(constructor: Function) {
   Object.seal(constructor.prototype)
 }
 
+function checkValidPokemonId(){
+  return function(target: any, propertyKey: string, descriptor: PropertyDescriptor){
+    const originalMethod = descriptor.value;
+    descriptor.value = (id: number) => {
+      if (id < 1 || id > 800)
+        return console.error('el id del pokemon debe estar entre 1 y 800');
+      else
+        return originalMethod(id);
+    }
+  }
+}
+
 @blockPrototype
-@printToConsoleConditional(true)
+// @printToConsoleConditional(true)
 export class Pokemon{
 
   public publicAPI: string = 'https://pokeapi.co'
@@ -24,4 +36,10 @@ export class Pokemon{
   constructor(
     public name: string
   ){}
+
+  @checkValidPokemonId()
+  savePokemonToDB(id: number){
+    console.log(`Pokemon save at DB with ID: ${id}`);
+    
+  }
 }
--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -2,6 +2,6 @@ import { Pokemon } from "./decorators/pokemon-class";
 
 const charmander = new Pokemon('charmander');
 
-(Pokemon.prototype as any).customName = 'Pikachu';
+// (Pokemon.prototype as any).customName = 'Pikachu';
 
-console.log(charmander);
+charmander.savePokemonToDB(1212);
